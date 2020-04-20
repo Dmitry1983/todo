558d1d315abb6005be4ab8d273c969c4953b0772
Dmitriy <barbash.dv@yandex.com>
2020-04-20 19:27:31 +0300

    add react-native-vector-icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { THEME } from '../theme'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 export const Navbar = (props) => {  //(props)  => ({title})
+    const myIcon = <Icon name="check-circle" size={30} color={THEME.COLOR.GREY} />;
     return (
         <View style={styles.navbar}>
-            <Text style={styles.text}>{props.title}</Text>
+            <Text style={styles.text}>{myIcon}  {props.title}</Text>
         </View>
     )
 }
