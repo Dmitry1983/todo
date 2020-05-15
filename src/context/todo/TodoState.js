4e7279dc0b062f050a0a47ef3cd99e5f181b3268
Dmitriy <barbash.dv@yandex.com>
2020-05-15 13:00:44 +0300

    055

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -2,8 +2,10 @@ import React, { useReducer, useContext } from 'react'
 import { Alert } from 'react-native'
 import { TodoContext } from './todoContext'
 import { todoReducer } from './todoReducer'
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO, SHOW_LOADER, HIDE_LOADER, SHOW_ERROR, CLEAR_ERROR } from '../types'
+import { ADD_TODO, REMOVE_TODO, UPDATE_TODO, SHOW_LOADER, HIDE_LOADER, SHOW_ERROR, CLEAR_ERROR, FETCH_TODOS } from '../types'
 import { ScreenContext } from '../screen/screenContext'
+
+const url = 'https://rn-todo-app-4c535.firebaseio.com/'
 
 export const TodoState = ({ children }) => {
 
@@ -23,7 +25,28 @@ export const TodoState = ({ children }) => {
 
 
     // @ts-ignore
-    const addTodo = title => dispatch({ type: ADD_TODO, title })
+    const addTodo = async title => {
+        const response = await fetch(url + 'todos.json', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title })
+        })
+        const data = await response.json()
+        const id = data.name
+        console.log('data: ', data.name)
+        dispatch({ type: ADD_TODO, title, id })
+    }
+
+    const fetchTodos = async () => {
+        const response = await fetch(url + 'todos.json', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        })
+        const data = await response.json()
+        console.log('FETCH DATA: ', data)
+        const todos = Object.keys(data).map(key => ({ ...data[key], id: key }))
+        dispatch({ type: FETCH_TODOS, todos })
+    }
 
 
     const removeTodo = id => {
@@ -42,6 +65,7 @@ export const TodoState = ({ children }) => {
                     style: 'destructive',
                     onPress: () => {
                         changeScreen(null)
+                        // @ts-ignore
                         dispatch({ type: REMOVE_TODO, id })
                     }
                 },
@@ -53,21 +77,28 @@ export const TodoState = ({ children }) => {
     // @ts-ignore
     const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title })
 
+    // @ts-ignore
     const showLoader = () => dispatch({ type: SHOW_LOADER })
 
+    // @ts-ignore
     const hideLoader = () => dispatch({ type: HIDE_LOADER })
 
+    // @ts-ignore
     const showError = (error) => dispatch({ type: SHOW_ERROR, error })
 
+    // @ts-ignore
     const clearError = () => dispatch({ type: CLEAR_ERROR })
 
     return (
         <TodoContext.Provider
             value={{
                 todos: state.todos,
+                loading: state.loading,
+                error: state.error,
                 addTodo,
                 removeTodo,
-                updateTodo
+                updateTodo,
+                fetchTodos
             }}
         >
             {children}
--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -10,11 +10,11 @@ import {
 } from "../types"
 
 const handlers = {
-    [ADD_TODO]: (state, { title }) => ({
+    [ADD_TODO]: (state, { title, id }) => ({
         ...state,
         todos: [
             ...state.todos, {
-                id: Date.now().toString(),
+                id: id,
                 title: title
             }]
     }),
