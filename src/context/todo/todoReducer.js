b94b72763438d75a254ffe2eeb54331bb9b741aa
Dmitriy <barbash.dv@yandex.com>
2020-05-15 12:25:36 +0300

    053

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,4 +1,13 @@
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "../types"
+import {
+    ADD_TODO,
+    REMOVE_TODO,
+    UPDATE_TODO,
+    SHOW_LOADER,
+    HIDE_LOADER,
+    SHOW_ERROR,
+    CLEAR_ERROR,
+    FETCH_TODOS
+} from "../types"
 
 const handlers = {
     [ADD_TODO]: (state, { title }) => ({
@@ -20,6 +29,12 @@ const handlers = {
             return todo
         })
     }),
+    [SHOW_LOADER]: state => ({ ...state, loading: true }),
+    [HIDE_LOADER]: state => ({ ...state, loading: false }),
+    [CLEAR_ERROR]: state => ({ ...state, error: null }),
+    [SHOW_ERROR]: (state, { error }) => ({ ...state, error }),
+    [FETCH_TODOS]: (state, { todos }) => ({ ...state, todos }),
+
     DEFAULT: state => state
 }
 
@@ -27,28 +42,3 @@ export const todoReducer = (state, action) => {
     const hadler = handlers[action.type] || handlers.DEFAULT
     return hadler(state, action)
 }
-
-// export const todoReducer = (state, action) => {
-
-//     switch (action.type) {
-//         case ADD_TODO: return {
-//             ...state,
-//             todos: [
-//                 ...state.todos, {
-//                     id: Date.now().toString(),
-//                     title: action.title
-//                 }]
-//         }
-//         case REMOVE_TODO: return { ...state, todos: state.todos.filter(todo => todo.id !== action.id) }
-
-//         case UPDATE_TODO: return {
-//             ...state, todos: state.todos.map(todo => {
-//                 if (todo.id === action.id) {
-//                     todo.title = action.title
-//                 }
-//                 return todo
-//             })
-//         }
-//         default: return state
-//     }
-// }
